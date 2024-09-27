d39569f381b81e27825cf50c643cf63d9931f69b
SafdarAliShah <programmersafdar@live.com>
2024-09-26 19:25:36 -0700

    branch updatUpdatee1.1

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+export default function Notfound() {
+  return (
+    <>
+      <section className="">
+        <div>404</div>
+        <div></div>
+        <div>Opps.. Page Not Found.</div>
+      </section>
+    </>
+  );
+}
